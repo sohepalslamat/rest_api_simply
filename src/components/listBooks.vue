1f35994e18e504e7ba9da685f7eb16d9dd6ba3e6
sohep alslamat <sohep.alslamat91@gmail.com>
2020-08-03 18:47:40 +0300

    method PATCH

--- src/components/listBooks.vue
+++ src/components/listBooks.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
       <ul>
-        <div>
-          <li v-for="book in books" :key="book.id">
-            <h3>name: {{book.name}}</h3>
-            <h4>price: {{book.price}}</h4>
+        <li v-for="book in books" :key="book.id">
+            <h3>name: 
+              <span @dblclick="show=false" v-if="show">{{book.name}}</span> 
+              <input v-model="book.name" @keyup.enter="updateBook(book.id,'name', book.name)" v-if="!show" type="text">
+            </h3>
+            <h4>price: 
+              <span @dblclick="show1=false" v-if="show1">{{book.price}}</span> 
+              <input v-model="book.price" @keyup.enter="updateBook(book.id,'price', book.price)" v-if="!show1" type="text">
+            </h4>
             <router-link class="nav-link" :to="{name: 'edit_book', params:{id:book.id}}">Edit</router-link>
-          </li>
-          
-        </div>
+          ---------------
+        </li>
     </ul>
   </div>
 </template>
@@ -20,18 +24,35 @@ export default {
   name: 'App',
   data(){
     return{
-      books:[]    
+      books:[],
+      show:true,
+      show1:true    
     }
   },
   created(){
     axios.get('http://sohep91.pythonanywhere.com/books/').then((res)=>{
       this.books = res.data
     })
+  },
+  methods:{
+    updateBook(id, item, value) {
+      axios.patch(`http://sohep91.pythonanywhere.com/books/${id}/?${item}=${value}`).then((res) => {
+        console.log(res.status)
+        if(item == 'name'){
+          this.show = true
+        } else this.show1 = true
+        
+      })
+    }
+
   }
   
 }
 </script>
 
 <style>
+  ul{
+    list-style-type: none;
+  }
 
 </style>
